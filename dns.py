28c23f92121b60b02b4f64a02301f9a0ee417601
wgp <sanguineskralx@gmail.com>
2018-03-27 11:48:07 -0700

    added 2.11 to the dns.py and ran the program creating the .ioe files

--- dns.py
+++ dns.py
@@ -1,22 +1,32 @@
 import os
 def main():	
-	url = ['williamp', 'bravo',   'Micahb345' ,'bipolar2','cynthia'
-		   'keegy',    'spider',  'amospia',   'komodo',  'h27'
-		   'shew',     'croisant','pmoscar',   'papa15',  'bryan']
-	ip = ['10.183.2.1',  '10.183.2.2',  '10.183.2.3'   '10.183.2.4',  '10.183.2.5'
-		  '10.183.2.6',  '10.183.2.8',  '10.183.2.9',  '10.183.2.10', '10.183.2.12'
-		  '10.183.2.13', '10.183.2.14', '10.183.2.15', '10.183.2.16', '10.183.2.20']
+	url = ['williamp', 'bravo',   'Micahb345' ,'bipolar2','cynthia',
+		   'keegy',    'spider',  'amospia',   'komodo',  'h27',
+		   'shew',     'croisant','pmoscar',   'papa15',  'bryan', 'p51']
+	ip = ['10.183.2.1',  '10.183.2.2',  '10.183.2.3',   '10.183.2.4',  '10.183.2.5',
+		  '10.183.2.6',  '10.183.2.8',  '10.183.2.9',  '10.183.2.10', '10.183.2.12',
+		  '10.183.2.13', '10.183.2.14', '10.183.2.15', '10.183.2.16', '10.183.2.20', '10.183.2.11']
 	print (url)
 	print(ip)
-	urlstring1 = url[0]+'.ioe'
-	urlstring2 = 'www'+url[0]+'.ioe'
-	urlstring3 = 'video'+url[0]+'.ioe'
-	cmd1 = 'touch '+ urlstring1
-	os.system(cmd1)
-	echo1 = 'address=/'+urlstring1+'/'+ip[0]
-	cmd2 = 'echo "'+echo1+'" >>'+urlstring1
-	print(cmd2)
-	os.system(cmd2)
+	length=len(url);
+	for i in range(length):
+		urlstring1 = url[i]+'.ioe'
+		urlstring2 = 'www.'+url[i]+'.ioe'
+		urlstring3 = 'video.'+url[i]+'.ioe'
+		cmd1 = 'touch '+ urlstring1
+		os.system(cmd1)
+		echo1 = 'address=/'+urlstring1+'/'+ip[i]
+		cmd2 = 'echo "'+echo1+'" >>'+urlstring1
+		echo2 = 'address=/'+urlstring2+'/'+ip[i]
+		cmd3 = 'echo "'+echo2+'" >>'+urlstring1
+		echo3 = 'address=/'+urlstring3+'/'+ip[i]
+		cmd4 = 'echo "'+echo3+'" >>'+urlstring1
+		print(cmd2)
+		os.system(cmd2)
+		print(cmd3)
+		os.system(cmd3)
+		print(cmd4)
+		os.system(cmd4)
 
 main()
 
